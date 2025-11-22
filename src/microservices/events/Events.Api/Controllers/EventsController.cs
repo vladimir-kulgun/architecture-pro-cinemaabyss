1039e4709040482fae555a72efb8b7fd1f47840a
Vladimir Kulgun <koulgoun@yandex.ru>
2025-11-22 18:13:35 +0200

    fix failing test

--- src/microservices/events/Events.Api/Controllers/EventsController.cs
+++ src/microservices/events/Events.Api/Controllers/EventsController.cs
@@ -52,7 +52,7 @@ namespace Events.Api.Controllers
         [HttpGet("health")]
         public async Task<IActionResult> HealthCheck()
         {
-            return Ok();
+            return Ok("Ok");
         }
     }
 }
