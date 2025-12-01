e0d96a1583b5a174805eb72187f320915210c7f9
Vladimir Kulgun <koulgoun@yandex.ru>
2025-12-01 15:28:43 +0200

    Задание 4. Реализация Helm-чартов. запуск тестов

--- cinemaabyss-tests.bat
+++ cinemaabyss-tests.bat
@@ -1,3 +1,5 @@
 cls
-docker build -t cinemaabyss-tests -f .\tests\postman\Dockerfile .\tests\postman\
-docker run cinemaabyss-tests --environment kubernetes
+rem docker build -t cinemaabyss-tests -f .\tests\postman\Dockerfile .\tests\postman\
+rem docker run --add-host cinemaabyss.example.com:172.17.0.1 cinemaabyss-tests --environment local
+
+npm run test:kubernetes
