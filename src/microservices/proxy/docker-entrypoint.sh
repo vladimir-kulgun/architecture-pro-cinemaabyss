7c344bfc973cedd504614ae00226a109b35e43c7
Vladimir Kulgun <koulgoun@yandex.ru>
2025-12-01 22:21:47 +0200

    typos

--- src/microservices/proxy/docker-entrypoint.sh
+++ src/microservices/proxy/docker-entrypoint.sh
@@ -99,18 +99,18 @@ cat >> $NGINX_CONF <<EOF
 
         location /api/payments {
           proxy_pass http://${MONOLITH_HOST};
-          proxy_set_header Host $host;
-          proxy_set_header X-Real-IP $remote_addr;
-          proxy_set_header X-Forwarded-For $proxy_add_x_forwarded_for;
-          proxy_set_header X-Forwarded-Proto $scheme;
+          proxy_set_header Host \$host;
+          proxy_set_header X-Real-IP \$remote_addr;
+          proxy_set_header X-Forwarded-For \$proxy_add_x_forwarded_for;
+          proxy_set_header X-Forwarded-Proto \$scheme;
         }
 
         location /api/subscriptions {
           proxy_pass http://${MONOLITH_HOST};
-          proxy_set_header Host $host;
-          proxy_set_header X-Real-IP $remote_addr;
-          proxy_set_header X-Forwarded-For $proxy_add_x_forwarded_for;
-          proxy_set_header X-Forwarded-Proto $scheme;
+          proxy_set_header Host \$host;
+          proxy_set_header X-Real-IP \$remote_addr;
+          proxy_set_header X-Forwarded-For \$proxy_add_x_forwarded_for;
+          proxy_set_header X-Forwarded-Proto \$scheme;     
         }
     }
 }
